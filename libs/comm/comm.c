6818db792c3f832facd4a4e7570d69605124a08e
Nonoo <nonoo@nonoo.hu>
2015-05-28 21:33:20 +0200

    added proper pcap deinit, filtering

--- libs/comm/comm.c
+++ libs/comm/comm.c
@@ -3,6 +3,7 @@
 #include "comm.h"
 
 #include <libs/daemon/console.h>
+#include <libs/daemon/daemon-poll.h>
 #include <libs/config/config.h>
 
 #include <pcap/pcap.h>
@@ -18,6 +19,9 @@ void comm_process(void) {
 	const uint8_t *packet;
 	struct pcap_pkthdr pkthdr;
 
+	if (pcap_handle == NULL)
+		return;
+
 	packet = pcap_next(pcap_handle, &pkthdr);
 	if (packet != NULL)
 		comm_processpacket(packet, pkthdr.len);
@@ -26,26 +30,44 @@ void comm_process(void) {
 flag_t comm_init(void) {
 	char *netdevname = NULL;
 	char pcap_errbuf[PCAP_ERRBUF_SIZE] = {0,};
-	struct bpf_program fcode = {0,};
+	struct bpf_program pcap_filter = {0,};
+	int pcap_dev = -1;
+	char *pcap_filter_str = "ip and udp";
 
 	netdevname = config_get_netdevicename();
 
-	console_log("comm: opening capture device %s\n", netdevname);
+	console_log("comm: opening capture device %s, capture buffer size: %u\n", netdevname, BUFSIZ);
 
-	pcap_handle = pcap_open_live(netdevname, BUFSIZ, 1, 0, pcap_errbuf);
+	pcap_handle = pcap_open_live(netdevname, BUFSIZ, 1, -1, pcap_errbuf);
 	if (pcap_handle == NULL) {
 		console_log("comm error: couldn't open device %s: %s\n" , netdevname, pcap_errbuf);
 		free(netdevname);
 		return 0;
 	}
 	free(netdevname);
-	if (pcap_compile(pcap_handle, &fcode, "ip and udp", 1, PCAP_NETMASK_UNKNOWN) < 0) {
+	if (pcap_compile(pcap_handle, &pcap_filter, pcap_filter_str, 1, PCAP_NETMASK_UNKNOWN) < 0) {
 		console_log("comm error: can't init packet capture: %s\n", pcap_geterr(pcap_handle));
 		return 0;
 	}
+
+	if (pcap_setfilter(pcap_handle, &pcap_filter) < 0)
+		console_log("comm warning: can't set filter to \"%s\"\n", pcap_filter_str);
+
+	pcap_dev = pcap_get_selectable_fd(pcap_handle);
+	if (pcap_dev == -1)
+		console_log("comm warning: can't add pcap handle to the poll list\n");
+	else
+		daemon_poll_addfd_read(pcap_dev);
 
 	return 1;
 }
 
 void comm_deinit(void) {
+	int pcap_dev = -1;
+
+	pcap_dev = pcap_get_selectable_fd(pcap_handle);
+	if (pcap_dev > -1)
+		daemon_poll_removefd(pcap_dev);
+
+	pcap_close(pcap_handle);
 }
