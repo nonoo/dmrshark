a72ef02c724868195f6f4dc128846a9776c7be96
Nonoo <nonoo@nonoo.hu>
2015-11-04 21:38:58 +0100

    Upgraded version number to v2.0.0

--- config/defaults.h
+++ config/defaults.h
@@ -1,8 +1,8 @@
 #ifndef DEFAULTS_H_
 #define DEFAULTS_H_
 
-#define VERSION_MAJOR							1
-#define VERSION_MINOR							1
+#define VERSION_MAJOR							2
+#define VERSION_MINOR							0
 #define VERSION_PATCH							0
 
 #include "appids.h"
